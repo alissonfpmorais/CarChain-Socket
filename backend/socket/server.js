2e48ad18ce3f8df90d721141777cc2995831e571
Alisson Morais <alissonfpmorais@gmail.com>
2018-08-17 10:32:04 -0300

    improve reconnection system

--- backend/socket/server.js
+++ backend/socket/server.js
@@ -60,11 +60,10 @@ function spreadTheWord(options, payload) {
     const nodes = options.nodesAsClient.concat(options.nodesAsServer)
     const nodesToConnect = getNodesToConnect(nodes, payload)
 
-    if(nodesToConnect.length > 0) {
-        options.connectToPool(nodesToConnect)
-        options.clients.forEach(client => client.emit('nodes-to-connect', nodesToConnect))
-        options.server.emit('nodes-to-connect', nodesToConnect)
-    }
+    if(nodesToConnect.length > 0) options.connectToPool(nodesToConnect)
+
+    options.clients.forEach(client => client.emit('nodes-to-connect', nodesToConnect))
+    options.server.emit('nodes-to-connect', nodesToConnect)
 }
 
 function emitError(io, payload) {
