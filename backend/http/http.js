fe910a6d3b8c1167997448a64ed0822b7caf477c
Alisson Morais <alissonfpmorais@gmail.com>
2018-08-16 21:31:30 -0300

    change http server port from 3000 to 3002

--- backend/http/http.js
+++ backend/http/http.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000
+const port = 3002
 
 function run(getOptions) {
     app.get('/', (req, res) => {
